b5e441b01e41638838de66932ed826fc921255ee
Anastasia Nesteryukova <anastasia.nesteryukova@gmail.com>
2022-05-09 16:11:07 +1200

    Get spotify access token with async and window postMessage

--- src/util/Spotify.ts
+++ src/util/Spotify.ts
@@ -1,30 +1,50 @@
-let accessToken: string;
+let accessToken = localStorage.getItem('spotifyAccessToken');
 const client_id = 'c0ed2e64c73b4756b693beae2ed7a508';
 const redirect_uri = 'http://localhost:3000/';
 const authorize_url = 'https://accounts.spotify.com/authorize';
 
-const Spotify = {
-  getAccessTocken() {
-    if (!accessToken) {
-      let accessTokenMatch = window.location.href.match(/(?<=access_token=)[^&]+/);
-      let expiresInMatch = window.location.href.match(/(?<=expires_in=)[^&]+/);
-      if (accessTokenMatch && expiresInMatch) {
-        accessToken = accessTokenMatch[0];
-        const expiresIn = expiresInMatch[0];
-        window.setTimeout(() => accessToken = '', Number(expiresIn) * 1000);
-        window.history.pushState('Access Token', '', '/');
-      } else {
-        let url = `${authorize_url}?client_id=${client_id}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirect_uri}` as (string | Location) & Location;
-        window.location = url;
-      }
+window.onload = function () {
+  let tockenIsExpired = localStorage.getItem('spotifyTokenExpirationDate') && Number(localStorage.getItem('spotifyTokenExpirationDate')) <= Date.now();
+  if (tockenIsExpired) {
+    localStorage.clear();
+  }
+  if (!accessToken) {
+    let accessTokenMatch = window.location.href.match(/(?<=access_token=)[^&]+/);
+    let expiresInMatch = window.location.href.match(/(?<=expires_in=)[^&]+/);
+    if (accessTokenMatch && expiresInMatch) {
+      window.opener.postMessage({content: {token: accessTokenMatch, expires: expiresInMatch}, type: "token"}, 'http://localhost:3000/');
+      window.close();
     }
-    console.trace(accessToken);
+  }
+}
+
+
+const Spotify = {
+  async retrieveAccessTocken() {
+    let url = `${authorize_url}?client_id=${client_id}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirect_uri}`;
+    const popup = window.open(url, '_blank', 'popup') as Window;
+    return new Promise(resolve => {
+      window.addEventListener("message", listener, false);
+      function listener(event: MessageEvent) {
+        if (event.origin === 'http://localhost:3000' && event.source === popup && event.data.type === 'token') {
+          window.removeEventListener("message", listener)
+          resolve(event.data.content)
+        }
+      }
+    })
+
   },
 
-  search(term: string) {
-    console.trace(accessToken);
+  async search(term: string) {
+    //accessToken = localStorage.getItem('spotifyAccessToken');
     if (!accessToken) {
-      Spotify.getAccessTocken();
+      const tokenData = await Spotify.retrieveAccessTocken() as any;
+      accessToken = tokenData.token;
+      const expiresIn = tokenData.expires;
+      localStorage.setItem('spotifyAccessToken', `${accessToken}`);
+      localStorage.setItem('spotifyTokenExpirationDate', `${Date.now() + expiresIn * 1000}`);
+      window.setTimeout(() => localStorage.clear(), Number(expiresIn) * 1000);
+      window.history.pushState('Access Token', '', '/');  accessToken = localStorage.getItem('spotifyAccessToken');
     }
     return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
       headers: {
