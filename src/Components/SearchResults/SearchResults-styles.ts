5c5cb1f2599f5670dfa541506adf310e88fe73aa
Anastasia Nesteryukova <anastasia.nesteryukova@gmail.com>
2022-05-17 17:52:47 +1200

    Make search results component look similar to playlists component

--- src/Components/SearchResults/SearchResults-styles.ts
+++ src/Components/SearchResults/SearchResults-styles.ts
@@ -4,10 +4,15 @@ export const search_results = css`
   width: 50%;
   height: 600px;
   overflow-y: scroll;
-  padding: .88rem;
+  padding: 2.27rem 1.16rem;
   background-color: rgba(1, 12, 63, 0.7);
   box-shadow: 0 4px 2px 2px #000000;
   scrollbar-width: none;
+
+  h2 {
+    line-height: 45px;
+    border-bottom: 1px solid #6f6f6f;
+  }
 
   &::-webkit-scrollbar {
     width: 0;
